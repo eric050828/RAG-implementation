22d9057802dfe1d0dd2a3c29b45bfa3106b4ddb7
EricLEE0828 <ericlee050828@gmail.com>
2024-05-01 00:54:11 +0800

    Feat: Add chat delete button

--- pages/1_chat.py
+++ pages/1_chat.py
@@ -1,7 +1,7 @@
 from time import sleep
 
 import streamlit as st
-from utils import (list_files, save_session, load_session, ROOT_DIR)
+from utils import (empty_chat, list_files, save_session, load_session, ROOT_DIR)
 
 
 st.session_state.update(load_session())
@@ -14,15 +14,53 @@ with st.sidebar:
     st.page_link(page="pages/2_files.py", label="文件")
     st.divider()
 
+# Chat list
 chat_title = st.empty()
 chat_path = st.empty()
 
-selected_chat = st.sidebar.selectbox("chats", st.session_state.chats.keys())
+col1, col2 = st.sidebar.columns([3, 1])
+
+# Select chat
+selected_chat = col1.selectbox("chats", st.session_state.chats.keys())
 chat_title.header(selected_chat)
 
+# Chat Delete
+if col2.button("Delete", type="primary"):
+    del st.session_state.chats[selected_chat]
+    if not st.session_state.chats.keys():
+        st.session_state.chats.update(empty_chat("chat1"))
+    selected_chat = list(st.session_state.chats.keys())[0]
+    
+# Chat rename
+col1, col2 = st.sidebar.columns([3, 1])
+new_chat_name = col1.text_input("new name")
+if col2.button("Rename") and new_chat_name is not None:
+    st.session_state.chats[new_chat_name] = st.session_state.chats[selected_chat]
+    del st.session_state.chats[selected_chat]
+    selected_chat = new_chat_name
+    
+
+st.sidebar.divider()
+
+# Chat history
 for message in st.session_state.chats[selected_chat]["messages"]:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+
+# Select file
+path = ""
+if st.session_state.chats[selected_chat]["path"] != "":
+    path = st.session_state.chats[selected_chat]["path"]
+
+files = list_files("/data")
+col1, col2 = st.sidebar.columns([3, 1])
+selected_pdf = col1.selectbox("選擇文件", [None]+files, placeholder="選擇參考文件")
+submit_btn = col2.button("上傳")
+if submit_btn and selected_pdf is not None:
+    path = f"{ROOT_DIR}\\data\\{selected_pdf}"
+    st.session_state.chats[selected_chat]["path"] = path
+
+chat_path.write(path)
 
 # React to user input
 if prompt := st.chat_input("What is up?"):
@@ -37,15 +75,6 @@ if prompt := st.chat_input("What is up?"):
         
     st.session_state.chats[selected_chat]["messages"].append({"role": "assistant", "content": "".join(response)})
 
-# Select file
-files = list_files("/data")
-if st.session_state.chats[selected_chat]["path"] != "":
-    path = st.session_state.chats[selected_chat]["path"]
-else:
-    if selected_pdf:=st.sidebar.selectbox("選擇文件", [None]+files, placeholder="選擇參考文件"):
-        path = f"{ROOT_DIR}\\data\\{selected_pdf}"
-        st.session_state.chats[selected_chat]["path"] = path
-chat_path.write(path)
 
 
 save_session(st.session_state.to_dict())
--- utils/session.py
+++ utils/session.py
@@ -1,10 +1,19 @@
 from json import loads, dumps
 
 
-def save_session(data: dict):
+def save_session(data: dict) -> None:
     with open(".config\\session.json", "w") as f:
         f.write(dumps(data))
         
 def load_session():
     with open(".config\\session.json", "r") as f:
         return loads(f.read())
+
+def empty_chat(title: str):
+    chat = {
+        title: {
+            "path": "",
+            "messages": []
+        }
+    }
+    return chat
