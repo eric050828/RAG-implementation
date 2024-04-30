05c545080ccb5b56ea8141fd49d164bd915e7e54
EricLEE0828 <ericlee050828@gmail.com>
2024-04-30 20:30:00 +0800

    Add streamlit config

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 
 with st.sidebar:
-    st.sidebar.title("Chat with Docs")
+    st.title("Chat with Docs")
     
-    st.sidebar.page_link(page="pages/1_chat.py", label="聊天室")
-    st.sidebar.page_link(page="pages/2_files.py", label="文件")
+    st.page_link(page="pages/1_chat.py", label="聊天室")
+    st.page_link(page="pages/2_files.py", label="文件")
     
 
 st.header("Introduction")
